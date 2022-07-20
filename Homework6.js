238c85f38e2f8391b58330adb6fcd786c8ce162a
lusinekalashian <lusine.kalashian@gmail.com>
2022-07-20 15:40:00 +0400

    Homework 6

--- Homework6.js
+++ Homework6.js
@@ -1,20 +1,25 @@
 /* #1
 const x = [2,3,6,8,5,4,3];
-const y = [6,2];
+const y = [6];
 const z = [8,2,45,0];
 function maxProduct(arr){
-    currentMaxProduct = arr[0] * arr[1];
-    for (let i = 1;i < arr.length;i++){
-        if (arr[i] * arr[i + 1] > currentMaxProduct){
-            currentMaxProduct = arr[i] * arr[i + 1];
+    if (arr.length === 1){
+        return arr[0];
+    }else {
+        let currentMaxProduct = arr[0] * arr[1];
+
+        for (let i = 1; i < arr.length - 1; i++) {
+            if (arr[i] * arr[i + 1] > currentMaxProduct) {
+                currentMaxProduct = arr[i] * arr[i + 1];
+            }
         }
+        return currentMaxProduct;
     }
-    return currentMaxProduct;
 }
 console.log(maxProduct(x));
 console.log(maxProduct(y));
 console.log(maxProduct(z));*/
-/*#2
+/* #2
 const arr1 = [4,2,6,8,9,11,1];
 const arr2 = [2,6,7,8,9,4];
 function arrSequence(arr){
@@ -23,10 +28,7 @@ function arrSequence(arr){
     for (let i = 0; i < arr.length;i++){
         if (arr[i] > maxElem){
             maxElem = arr[i];
-        }
-    }
-    for (let i = 0; i < arr.length;i++){
-        if (arr[i] < minElem){
+        }else if (arr[i] < minElem){
             minElem = arr[i];
         }
     }
@@ -37,19 +39,19 @@ function arrSequence(arr){
 }
 console.log(arrSequence(arr1));
 console.log(arrSequence(arr2));*/
-/*#3
+/* #3
 const str1 = "Prison of War";         //"POW"
 const str2 = "Have a good night"      //"HAGN"
 function convertLongPhrase(longPhrase){
-    let acronym = [];
+    let acronym = "";
     for (let i = 0;i < longPhrase.length;i++){
         if (i === 0){
-            acronym.push(longPhrase[i]);
+            acronym = "" + longPhrase[i];
         }else if (longPhrase[i] === " "){
-            acronym.push(longPhrase[i + 1].toUpperCase());
+            acronym += "" + longPhrase[i + 1].toUpperCase();
         }
     }
-    return acronym.join("");
+    return acronym;
 
 }
 console.log(convertLongPhrase(str1));
